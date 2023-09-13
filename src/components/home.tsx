7c41ed8ad5c24782c6e73ebecdc47ba84f75d4df
Darwin Lantigua <darwinlf@outlook.com>
2023-09-12 20:20:57 -0400

    changing packages

---
+++ src/components/home.tsx
@@ -0,0 +1,12 @@
+import logo from '../images/logo.svg';
+
+const Home = () => {
+    return (
+        <header className="App-header">
+         <img src={logo} alt='logo' className='App-logo'></img>
+         <p>House of Projects</p>
+        </header>
+    );
+  };
+  
+  export default Home;
